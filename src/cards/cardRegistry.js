28a8be749b1323e482260e4f1452a36e848f0406
Andrew Bunn <andrew@bunn.nz>
2026-03-28 15:07:32 +1300

    feat: cleanup unused components etc

--- src/cards/cardRegistry.js
+++ src/cards/cardRegistry.js
@@ -1,10 +1,8 @@
 import { defineAsyncComponent } from 'vue'
 
 export const CARD_REGISTRY = {
-  //detailTiles:    defineAsyncComponent(() => import('./DetailTilesCard.vue')),
   combinedHourly: defineAsyncComponent(() => import('./HourlyForecastCard.vue')),
   dailyForecast:  defineAsyncComponent(() => import('./DailyForecastCard.vue')),
-  //hourlyStrip:    defineAsyncComponent(() => import('./HourlyStripCard.vue')),
   sunriseMoon:    defineAsyncComponent(() => import('./SunriseMoonCard.vue')),
   radar:          defineAsyncComponent(() => import('./RadarCard.vue')),
 }
